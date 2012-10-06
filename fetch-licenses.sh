84d576d65ed071da7783013a19a2cf58ba155147
Stephan Kulow <coolo@suse.de>
2012-10-06 15:08:07 +0200

    add a + variant for all spdx licenses

--- fetch-licenses.sh
+++ fetch-licenses.sh
@@ -1,5 +1,8 @@
 curl "https://docs.google.com/spreadsheet/pub?hl=en_US&hl=en_US&key=0AqPp4y2wyQsbdGQ1V3pRRDg5NEpGVWpubzdRZ0tjUWc&single=true&gid=0&output=txt" | grep -v "New format" > licenses_changes.txt
-for i in `w3m -dump -cols 1000 http://spdx.org/licenses/ | grep "License Text" | sed -e 's, *Y *License Text,,; s, *License Text,,; s,.* ,,;'`; do echo "$i	$i" >> licenses_changes.txt ; done
+for i in `w3m -dump -cols 1000 http://spdx.org/licenses/ | grep "License Text" | sed -e 's, *Y *License Text,,; s, *License Text,,; s,.* ,,;'`; do 
+	echo "$i	$i" >> licenses_changes.txt ; 
+	echo "$i+	$i+" >> licenses_changes.txt ;
+done
 sort -o licenses_changes.txt -u licenses_changes.txt
 ( echo "First line" ; cat licenses_changes.txt ) > t
 mv t licenses_changes.txt
